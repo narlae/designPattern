ca362d17871e69b020874c4020dce717e9458e18
narlae <kyjttaa12@gmail.com>
2022-11-01 23:23:00 +0900

    Adapter Pattern

---
+++ src/AdaptPattern/Duck.java
@@ -0,0 +1,7 @@
+package AdaptPattern;
+
+public interface Duck {
+    public void quack();
+
+    public void fly();
+}
---
+++ src/AdaptPattern/Turkey.java
@@ -0,0 +1,7 @@
+package AdaptPattern;
+
+public interface Turkey {
+    public void gobble();
+
+    public void fly();
+}
