74016184eebf28a1ee1abe654f9fc353fafd461f
narlae <kyjttaa12@gmail.com>
2022-11-10 22:18:18 +0900

    IteratorPattern 2

--- src/IteratorPattern/Waitress.java
+++ src/IteratorPattern/Waitress.java
@@ -5,26 +5,34 @@ import java.util.Iterator;
 public class Waitress {
     Menu pancakeHouseMenu;
     Menu dinerMenu;
+    Menu cafeMenu;
 
-    public Waitress(Menu pancakeHouseMenu, Menu dinerMenu) {
+
+
+    public Waitress(Menu pancakeHouseMenu, Menu dinerMenu, Menu cafeMenu) {
         this.pancakeHouseMenu = pancakeHouseMenu;
         this.dinerMenu = dinerMenu;
+        this.cafeMenu = cafeMenu;
     }
 
     public void printMenu() {
         Iterator<MenuItem> pancakeIterator = pancakeHouseMenu.createIterator();
         Iterator<MenuItem> dinerIterator = dinerMenu.createIterator();
+        Iterator<MenuItem> cafeIterator = cafeMenu.createIterator();
+
         System.out.println("MENU\n----\n아침 식사");
         printMenu(pancakeIterator);
         System.out.println("\n점심 식사");
         printMenu(dinerIterator);
+        System.out.println("\n저녁 메뉴");
+        printMenu(cafeIterator);
     }
 
     private void printMenu(Iterator<MenuItem> iterator) {
         while (iterator.hasNext()) {
             MenuItem menuItem = iterator.next();
-            System.out.println(menuItem.getName()+", ");
-            System.out.println(menuItem.getPrice() + " -- ");
+            System.out.print(menuItem.getName()+", ");
+            System.out.print(menuItem.getPrice() + " -- ");
             System.out.println(menuItem.getDescription());
         }
     }
